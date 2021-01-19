79a9714ca3f6c7ee106e86d25b1870ec957b8ca7
Nacho <7736020@ufv.es>
2021-01-19 19:15:32 +0100

    Añadimos test mediante la función integrada en el IDE

---
+++ src/test/java/org/dis/back/EmpleadoBRTest.java
@@ -0,0 +1,30 @@
+package org.dis.back;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EmpleadoBRTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void calculaSalarioBruto() {
+
+        // fail("Esto es un error a proposito");
+
+        assertTrue("Este si pasa", true);
+    }
+
+    @Test
+    public void calculaSalarioNeto() {
+    }
+}
